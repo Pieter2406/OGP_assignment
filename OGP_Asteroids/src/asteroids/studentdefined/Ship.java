4e9b5f42123860ad4b32531226810f6003eab1dd
Pieter2406 <pieterverlinden2406@hotmail.com>
2013-03-14 05:32:04 +0100

    Merge methods in ship class

--- OGP_Asteroids/src/asteroids/studentdefined/Ship.java
+++ OGP_Asteroids/src/asteroids/studentdefined/Ship.java
@@ -18,27 +18,7 @@ import be.kuleuven.cs.som.annotate.*;
  */
 public class Ship implements IShip {
 
-	/**
-	 * Initialize the position, the radius and the angle of this ship with respectively the given position, radius and angle. 
-	 * Initialize the velocity of this ship to zero.
-	 * @param 	x
-	 * 			The position on the x-as for this new ship.
-	 * @param 	y 
-	 * 			The position on the y-as for this new ship.
-	 * @param	radius
-	 * 			The radius for this new ship.
-	 * @param 	angle
-	 * 			The angle for this new ship.
-	 * @param 	minimumRadius
-	 * 			The minimum radius for this new ship.
-	 * @pre		The given radius must be a valid radius
-	 * 			| isValidRadius(radius)
-	 * @pre 	The given minimumradius must be a valid minimumradius
-	 * 			| isValidMinimumRadius(minimumRadius)
-	 * @post		The Position, the radius and the angle of this ship are equal to respectively the given position, the given radius and the given angle.
-	 * 			The speed of this ship is initialized to zero.
-	 * @note		Speed will always be zero at the creation of an object, it is therefore not included as a parameter.
-	 */
+	//TODO: write doc
 	public Ship(double x, double y, double radius, double minimumRadius, double angle){
 		this.position = new Coordinate(x,y);
 		this.radius = radius; // no setRadius as it is final, only initialized once.
@@ -58,13 +38,11 @@ public class Ship implements IShip {
 	 * 			The radius for this new ship.
 	 * @param 	angle
 	 * 			The angle for this new ship.
-	 * @param 	minimumRadius
-	 * 			The minimum radius for this new ship.
 	 * @pre		The given radius must be a valid radius
 	 * 			| isValidRadius(radius)
-	 * @effect 	This new ship is initialized with a given x coordinate, a given y coordinate,
-	 * 			a radius, a minimum radius equal to zero and a given angle.
-	 * 			| this(x,y,radius, 0, angle)
+	 * @post		The Position, the radius and the angle of this ship are equal to respectively the given position, the given radius and the given angle.
+	 * 			The speed of this ship is initialized to zero.
+	 * @note		Speed will always be zero at the creation of an object, it is therefore not included as a parameter.
 	 */
 	public Ship(double x, double y, double radius, double angle){
 		this(x,y,radius, 0, angle);
@@ -90,11 +68,20 @@ public class Ship implements IShip {
 	 * 			The amount of velocity added to the current velocity in Km/h.
 	 * 
 	 * @post	If the given amount is higher than zero, this amount is added to the current velocity. If the new velocity 
-	 * 			exceeds the speed of light (300'000 Km/s), it will take on the value of the speed of light.
+	 * 			exceeds the speed of light, it will take on the value of the speed of light.
+	 * 			|if(new.getVelocity > getVelocity().SPEED_OF_LIGHT)
+	 * 			|	new.getVelocity == getVelocity().SPEED_OF_Light
 	 * 
 	 */
 	public void thrust(double amount){
-		//TODO: write method thrust
+		double checkedAmount = 0.0;
+		if(amount < 0){
+			checkedAmount = 0;
+		}
+		getVelocity().setVelocity	(	getVelocity().getVelocityX() + checkedAmount * Math.cos(getAngle())
+										,
+										getVelocity().getVelocityY() + checkedAmount * Math.sin(getAngle())
+									);
 	}
 
 	/**
@@ -129,15 +116,9 @@ public class Ship implements IShip {
 		this.angle = angle;
 	}
 
-	/**
-	 * Turn the ship into another direction.
-	 * @param 	angle
-	 * 			The given angle to be added to the current angle.
-	 * @effect 	Set the angle to the current angle plus the given angle.
-	 * 			| setAngle(this.getAngle() + angle)
-	 */
+	//TODO: write doc
 	public void turn(double angle){
-		setAngle(getAngle() + angle);
+		//TODO: write method turn
 	}
 
 	/**
@@ -153,22 +134,37 @@ public class Ship implements IShip {
 	}
 
 	/**
-	 * Sets the new position of this ship based on a given x and y coordinate.
+	 * Set the new position of this ship based on a given x and y coordinate.
 	 * @param 	x
 	 * 			Holds the new x coordinate for this ship.
 	 * @param 	y
 	 * 			Holds the new y coordinate for this ship.
-	 * @throws IllegalValueException 
-	 * @post 	The position of this ship is changed according to the given x and y coordinates.
+	 * @effect	If the given x and y values are valid numbers, the x and y values of this 
+	 * 			ships coordinates are set to the given x and y values.
 	 */
 	@Raw
 	public void setPosition(double x, double y){
-		this.position.transformTo(x,y); // values will be checked in the coordinate class.
+		getPosition().transformTo(x,y); // values will be checked in the coordinate class.
 	}
 
-	//TODO: defensive documentation
-	public void move(double time){
-		//TODO: write method move
+	/**
+	 * Move this ship in its current direction for a given amount of time.
+	 * @param 	duration
+	 * 			The duration of how long the ship moves in its current direction.
+	 * @effect	The position of the ship is set to its new position according
+	 * 			to the time it would have moved.
+	 * 			|setPosition(newPositionX,newPositionY)
+	 * @throws	IllegalValueException
+	 * 			The given duration is not a valid duration.
+	 * 			|(duration < 0)
+	 */
+	public void move(double duration){
+		if(duration < 0){
+			throw new IllegalValueException(duration);
+		}
+		double newPositionX = getPosition().getX() + (duration * getVelocity().getVelocityX());
+		double newPositionY = getPosition().getY() + (duration * getVelocity().getVelocityY());
+		setPosition(newPositionX,newPositionY); 
 	}
 
 	/**
@@ -176,104 +172,29 @@ public class Ship implements IShip {
 	 */
 	private Coordinate position;
 
-	/**
-	 * Return the distance between this ship and another given ship.
-	 * @param 	other
-	 * 			A given ship used to calculate a distance between it and this ship.
-	 * @throws  IllegalArgumentException
-	 * 			The other ship is not effective.
-	 * 			| other == null
-	 * @return  If the given ship is this ship 0.0 is returned, 
-	 * 			if the given ship is another ship the distance between both ships's coordinates is computed
-	 * 			and the radius of both ships is then subtracted from this value.
-	 * 			| if (other == this)
-	 * 			| then 0.0
-	 * 			| else getPosition().getDistanceBetween(other.getPosition()) - this.getRadius() - other.getRadius()
-	 */
-	public double getDistanceBetween(Ship other) throws IllegalArgumentException{
-		// Interface Iship is not yet implemented, if Iship is implemented the type of other should change to Iship.
-		try {
-			if (other == this)
-				return 0.0;
-			double rawDistance = getPosition().getDistanceBetween(other.getPosition());
-			double actualDistance = rawDistance - getRadius() - other.getRadius();
-			return actualDistance;
-		}
-		catch (NullPointerException excError){
-			assert (other == null);
-			throw new IllegalArgumentException ("Not an initialized given ship");
-		}
+
+	//TODO: defensive documentation{
+	public double getDistanceBetween(Ship other){
+		throw new RuntimeException("NOT IMPLEMENTED");
+		//TODO: write getter getDistanceBetween
 	}
 
-	/**
-	 * Return true if this ship overlaps with the given ship.
-	 * @param 	other
-	 * 			A given ship used to check if it overlaps with this ship.
-	 * @throws  IllegalArgumentException
-	 * 			The other ship is not effective.
-	 * 			| other == null
-	 * @return  True if and only if the given ship is the same as this ship,
-	 * 			or the distance between both ships is smaller than zero.
-	 * 			| result == 
-	 * 			|	other == this
-	 * 			|	|| getDistanceBetween(other) < 0
-	 */
 	public boolean overlap(Ship other){
-		try {
-			if (other == this)
-				return true;
-			if (getDistanceBetween(other) < 0)
-				return true;
-			return false;
-		}
-		catch (NullPointerException excError){
-			assert (other == null);
-			throw new IllegalArgumentException ("Not an initialized given ship");
-		}
+		throw new RuntimeException("NOT IMPLEMENTED");
+		//TODO: write method overlap
 	}
 
 	public double getTimeToCollision(Ship other){
-		throw new RuntimeException("NOT IMPLEMENTED");
-		//TODO: write getter getTimeToCollision
+		
+		
 	}
 
-	/**
-	 * Calculates the position of collision between this ship and a given ship
-	 * @param 	other
-	 * 			A given ship used to calculate the position of collision with this ship.
-	 * @throws  IllegalArgumentException
-	 * 			The other ship is not effective.
-	 * 			| other == null
-	 * @return  the Coordinate of collision between both ships, if the ships don't collide
-	 * 			return a null pointer.
-	 * 			| if (colx == Double.POSITIVE_INFINITY)
-	 * 			| 	then null
-	 * 			| 	else Coordinate(getPosition().getX() + getVelocity().getVelocityX() * getTimeToCollision(other)
-	 * 			|	,getPosition().getY() + getVelocity().getVelocityY() * getTimeToCollision(other))
-	 */
-	public Coordinate getCollisionPosition(Ship other) throws IllegalArgumentException  {
-		try {
-			double colX = getPosition().getX() + getVelocity().getVelocityX() * getTimeToCollision(other);
-			double colY = getPosition().getY() + getVelocity().getVelocityY() * getTimeToCollision(other);
-			if (colX == Double.POSITIVE_INFINITY) // one check is enough
-				return null;
-			else 
-				return new Coordinate(colX,colY);
-		}
-		catch (NullPointerException excError){
-			assert (other == null);
-			throw new IllegalArgumentException ("Not an initialized given ship");
-		}
+	public double getCollisionPosition(Ship other){
+		throw new RuntimeException("NOT IMPLEMENTED");
+		//TODO: write getter getCollisionPosition
 	}
 	//}
 
-	/**
-	 * Return the current radius.
-	 */
-	@Basic @Raw
-	private double getRadius() {
-		return this.radius;
-	}
 	/**
 	 * Check whether the given radius is a valid radius.
 	 * @param 	radius
