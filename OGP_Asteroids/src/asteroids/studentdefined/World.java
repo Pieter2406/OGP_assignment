4a1f2e2ef0facb9172fa7a919429b44708e230ef
Pieter2406 <pieterverlinden2406@hotmail.com>
2013-03-20 23:32:56 +0100

    Add some comment

--- OGP_Asteroids/src/asteroids/studentdefined/World.java
+++ OGP_Asteroids/src/asteroids/studentdefined/World.java
@@ -2,4 +2,22 @@ package asteroids.studentdefined;
 
 public class World {
 //TODO: write class world
+	/*
+	 * FULLY DEFENSIVE!
+	 * 
+	 * Contains following Objects:
+	 		* Ship
+	  		* Asteroid
+	  		* Bullet
+	 * Contains following variables:
+	 		* width
+	 		* height
+	 		* maxWidthHeight = Double.MaxValue
+	 * Contains following methods:
+	 		* addShip(Coordinate initLocation, Velocity initVelocity, double angle)
+	 		* addShip(double x, double y, double xVelocity, double yVelocity, double angle)
+	 		* ...  
+	 */
+	
+	
 }
